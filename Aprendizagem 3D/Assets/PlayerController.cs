583842ac42f13957e0d5b07f2f97b8cb7917a756
IgorPonciano <igorponciano.work@gmail.com>
2021-02-24 20:48:39 -0300

    AAAAAAAA
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Aprendizagem 3D/Assets/PlayerController.cs
+++ Aprendizagem 3D/Assets/PlayerController.cs
@@ -8,6 +8,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float mouseSensitivity;
     [SerializeField] bool lookCursor = true;
     private CharacterController characterController;
+    private bool isGrounded;
+
+    [Header("Ground Check stuff")]
+    public Transform groundcheck;
+    private float castRadius = 0.4f;
+    public LayerMask whatIsGround;
+
+    private float gravity = -9.8f;
 
     float cameraPitch = 0f;
     float walkSpeed = 6.0f;
@@ -48,11 +56,15 @@ public class PlayerController : MonoBehaviour
 
     private void UpdateMovement()
     {
+        isGrounded = true;
         Vector2 inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         inputDir.Normalize();
 
-        Vector3 velociy = (transform.forward * inputDir.y + transform.right * inputDir.x) * walkSpeed;
+        Vector3 velocity = (transform.forward * inputDir.y + transform.right * inputDir.x) * walkSpeed;
+        velocity += Vector3.up * gravity * Time.deltaTime* 100;
+
+
         //When a GameObject is rotated, the blue arrow representing the Z axis of the GameObject also changes direction.
-        characterController.Move(velociy * Time.deltaTime);
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
